985d37eabd26718c6e620bbfb083d6fed80a69c4
shreeup <shree912@yahoo.com>
2023-10-01 15:26:10 -0700

    fix defect

--- 01-node-tutorial/answers/11-fs-async.js
+++ 01-node-tutorial/answers/11-fs-async.js
@@ -5,32 +5,30 @@ fs.writeFile("./temporary/fileB.txt", "Line1 \n", (err, result) => {
   if (err) {
     console.log("This error happened: ", err);
   } else {
-    // here you write your next line
+    fs.writeFile(
+      "./temporary/fileB.txt",
+      "Line2 \n",
+      { flag: "a+" },
+      (err, result) => {
+        console.log("at point 2");
+        if (err) {
+          console.log("This error happened: ", err);
+        } else {
+          fs.writeFile(
+            "./temporary/fileB.txt",
+            "Line3 \n",
+            { flag: "a+" },
+            (err, result) => {
+              console.log("at point 3");
+              if (err) {
+                console.log("This error happened: ", err);
+              } else {
+                // here you write your next line
+              }
+            }
+          );
+        }
+      }
+    );
   }
 });
-fs.writeFile(
-  "./temporary/fileB.txt",
-  "Line2 \n",
-  { flag: "a+" },
-  (err, result) => {
-    console.log("at point 2");
-    if (err) {
-      console.log("This error happened: ", err);
-    } else {
-      // here you write your next line
-    }
-  }
-);
-fs.writeFile(
-  "./temporary/fileB.txt",
-  "Line3 \n",
-  { flag: "a+" },
-  (err, result) => {
-    console.log("at point 3");
-    if (err) {
-      console.log("This error happened: ", err);
-    } else {
-      // here you write your next line
-    }
-  }
-);
